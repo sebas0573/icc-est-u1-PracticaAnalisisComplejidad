e2442887f7e134bea2f9b81e8fe073f0c3a44d23
KarenS22 <karenquito2004@gmail.com>
2024-11-13 14:49:58 -0500

    Metodos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import java.util.Arrays;
+import java.util.Random;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        MetodosOrdenamiento metodos = new MetodosOrdenamiento();
+        
+
+        int[] sizes = {10, 20, 100, 1000, 5000, 10000, 30000};
+
+        System.out.println("Método Burbuja con Ajustes");
+        for (int size : sizes) {
+            int[] array = MetodosOrdenamiento.generateRandomArray(size);
+            int[] arrayCopy = Arrays.copyOf(array, array.length);
+            long startTime = System.nanoTime();
+            MetodosOrdenamiento.bubbleSort(arrayCopy);
+            long endTime = System.nanoTime();
+            System.out.printf("Con %d valores el tiempo es de %.6f seg.%n", size, (endTime - startTime) / 1e9);
+        }
+
+        System.out.println("\nMétodo Insercion");
+        for (int size : sizes) {
+            int[] array = MetodosOrdenamiento.generateRandomArray(size);
+            int[] arrayCopy = Arrays.copyOf(array, array.length);
+            long startTime = System.nanoTime();
+            MetodosOrdenamiento.insertionSort(arrayCopy);
+            long endTime = System.nanoTime();
+            System.out.printf("Con %d valores el tiempo es de %.6f seg.%n", size, (endTime - startTime) / 1e9);
+        }
+
+        System.out.println("\nMétodo Seleccion");
+        for (int size : sizes) {
+            int[] array = MetodosOrdenamiento.generateRandomArray(size);
+            int[] arrayCopy = Arrays.copyOf(array, array.length);
+            long startTime = System.nanoTime();
+            MetodosOrdenamiento.selectionSort(arrayCopy);
+            long endTime = System.nanoTime();
+            System.out.printf("Con %d valores el tiempo es de %.6f seg.%n", size, (endTime - startTime) / 1e9);
+        }
+
+
     }
+
+    
 }
---
+++ src/MetodosOrdenamiento.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+
+public class MetodosOrdenamiento {
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void bubbleSort(int[] array) {
+        boolean swapped;
+        int n = array.length;
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (array[i - 1] > array[i]) {
+                    int temp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = temp;
+                    swapped = true;
+                }
+            }
+            n--;
+        } while (swapped);
+    }
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static int[] generateRandomArray(int size) {
+    
+    Random rand = new Random();
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+        array[i] = rand.nextInt(1000);
+    }
+    return array;
+}
+    
+}
